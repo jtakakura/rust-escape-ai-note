cb29dd3fddd1f6b84115c8806e4abd4d45ac3547
Junji Takakura <j.takakura@gmail.com>
2023-07-22 07:31:34 +0900

    add configs

---
+++ src/configs.rs
@@ -0,0 +1,5 @@
+/// Window
+pub const WINDOW_BACKGROUND_COLOR: (u8, u8, u8, u8) = (36, 36, 36, 1);
+pub const IS_FULL_SCREEN: bool = true;
+pub const WINDOW_WIDTH: u32 = 1920;
+pub const WINDOW_HEIGHT: u32 = 1080;
