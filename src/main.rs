3e647fd5f5c967a7dd1d593b9efcbbb77eed2522
Junji Takakura <j.takakura@gmail.com>
2023-07-22 07:39:03 +0900

    create main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use escape::*;
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Escape".to_owned(),
+        window_width: WINDOW_WIDTH as i32,
+        window_height: WINDOW_HEIGHT as i32,
+        fullscreen: IS_FULL_SCREEN,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        let (r, g, b, a) = WINDOW_BACKGROUND_COLOR;
+        clear_background(Color::from_rgba(r, g, b, a));
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
+
+        next_frame().await
+    }
 }
