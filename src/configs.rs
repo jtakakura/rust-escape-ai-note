e213ce176890a202deadb33fe2cf93d17bb3e9c3
Junji Takakura <j.takakura@gmail.com>
2023-07-22 21:25:07 +0900

    add resources and levels to configs

--- src/configs.rs
+++ src/configs.rs
@@ -3,3 +3,17 @@ pub const WINDOW_BACKGROUND_COLOR: (u8, u8, u8, u8) = (36, 36, 36, 1);
 pub const IS_FULL_SCREEN: bool = true;
 pub const WINDOW_WIDTH: u32 = 1920;
 pub const WINDOW_HEIGHT: u32 = 1080;
+
+/// Resources
+pub const TILESET_PATH: &str = "tiled/tileset.png";
+pub const TILE_SET_NAME: &str = "tileset.png";
+
+/// Levels
+pub const LVL_BACKGROUND_SPRITE: &str = "tiled/lvl2.png";
+pub const LVL_MAP_PATH: &str = "tiled/lvl2.json";
+pub const LAYER_WALLS: &str = "walls";
+pub const LAYER_DOOR: &str = "door";
+pub const LAYER_KEYS: &str = "keys";
+pub const LAYER_PLAYER: &str = "player";
+pub const LAYER_SPIKES: &str = "spikes";
+pub const LAYER_ENEMIES: &str = "enemies";
